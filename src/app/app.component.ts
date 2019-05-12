a261fd616853b342f4a59c55a4b6409819b09fa6
Katrinape <katrina3447e@gmail.com>
2019-05-12 13:17:30 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  addTodoForm = this.fb.group({
+    title: [],
+    description: []
+  });
+
+  todos = [
+    { title: 'Todo 1', description: 'Desc 1' },
+    { title: 'Todo 2', description: 'Desc 2' },
+    { title: 'Todo 3', description: 'Desc 3' },
+    { title: 'Todo 4', description: 'Desc 4' }
+  ];
+
+  errMessage: string = '';
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+
+  onSubmit() {
+    const url = 'http://localhost:3000/validateTodo';
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const options = { headers: headers };
+    const todo = {
+      title: this.addTodoForm.controls['title'].value,
+      description: this.addTodoForm.controls['description'].value
+    };
+
+    this.http.post(url, todo, options).subscribe(
+      res => {
+        console.log(res)
+        this.todos.push(todo);
+        this.addTodoForm.reset();
+        this.errMessage = '';
+      },
+      err => {
+        console.log(err);
+        this.errMessage = err.error;
+      }
+    )
+  }
+
+  remove() {
+    this.todos.pop();
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatListModule, MatCardModule, MatIconModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
